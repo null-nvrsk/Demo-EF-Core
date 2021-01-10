3181b4a4f35a5fc7fbd73f7df2a4c6d8d7be08c2
NULL <nullnvrsk@gmail.com>
2021-01-10 22:08:12 +0300

    First real commit

---
+++ 2_ConnectToExistDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_ConnectToExistDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DemoEFCoreApp/Data/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using DemoEFCoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoEFCoreApp.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = demodb.sqlite");
+            optionsBuilder.LogTo(System.Console.WriteLine, LogLevel.Information);
+        }
+
+    }
+}
